da6e45aa96e274d8c3b499d564b971e9f4d39b4b
Glitch (abrupt-puzzle-marquis) <none>
2020-03-12 14:48:52 +0000

    🐕🏍 Updated with Glitch

--- bataille.js
+++ bataille.js
@@ -43,28 +43,36 @@ function render(){
 
 // récupérer le container
 var BoardContainer2 = document.getElementById("board2");
-BoardContainer2.innerHTML='';
 
-// Création de notre table de jeu dans le html
-let tableHtml2 = document.createElement("table");
-BoardContainer2.appendChild(tableHtml2);
-for (let i = 0; i < rows; i++) {
-  let tr = document.createElement("tr");
-  tableHtml2.appendChild(tr);
-  for (let j = 0; j < cols; j++) {
-    // on crée des td dans notre fichier html
-    let carre = document.createElement("td");
-    tr.appendChild(carre);
+function render2(){
+  BoardContainer2.innerHTML='';
+  let tableHtml2 = document.createElement("table");
+  BoardContainer2.appendChild(tableHtml2);
+  for (let i = 0; i < rows; i++) {
+    let tr = document.createElement("tr");
+    tableHtml2.appendChild(tr);
+    for (let j = 0; j < cols; j++) {
+      // on crée des td dans notre fichier html
+      let carre = document.createElement("td");
+      tr.appendChild(carre);
 
-    // pour chaque td qu'on crée, on lui donne deux attributs représentant son numéro de ligne et de colonne
-    carre.dataset.row = i;
-    carre.dataset.column = j;
+      // pour chaque td qu'on crée, on lui donne deux attributs représentant son numéro de ligne et de colonne
+      carre.dataset.row = i;
+      carre.dataset.column = j;
+      
+      if(table2[i][j] == -1)
+        carre.className = "manque";
+      else if(table2[i][j] == 2)
+        carre.className = "touche";
+      else if(table2[i][j] == 3)
+        carre.className = "coule";
 
-    //on défini pour chaque carré ses coordonnées en pixel d'après sa postition dans la matrice et on les envoie au css
-    /*var haut = j * squareSize;
-    var gauche = i * squareSize;
-    carre.style.top = haut + "px";
-    carre.style.left = gauche + "px";*/
+      //on défini pour chaque carré ses coordonnées en pixel d'après sa postition dans la matrice et on les envoie au css
+      /*var haut = j * squareSize;
+      var gauche = i * squareSize;
+      carre.style.top = haut + "px";
+      carre.style.left = gauche + "px";*/
+    }
   }
 }
 
@@ -112,6 +120,7 @@ var table2 = [
 
 
 render();
+render2();
 
 //On défini cet event listener sur Onclick pour lancer la fonction tirer dès qu'on click sur notre table de jeu
 BoardContainer.addEventListener("click", tirer, false);
@@ -202,4 +211,44 @@ function tirer(e) {
   }
 
   // si jamai on aura un problème par la suite on pourra essayé de stoper la propagation de cet évènement avec e.stopPropagation();
+}
+
+
+// fonction tirer, e est notre event et cette fonction sera appelée à chaque clic
+function tirer2(e) {
+  // sans ce if il n'ya pas du tout d'affichage, je n'ai pas compris pourquoi et j'ai trouvé ça sur stackoverflow maintenant ça marche mais je sais pas si il rique de poser problème par la suite
+  if ((e.target !== e.currentTarget) 
+      && ("row" in e.target.dataset) && ("column" in e.target.dataset)){   // Verifie si le clic se situe bien sur une case
+    //on extrait le numéro de ligne et de colonne depuis les attributs du carré sur lequel on a cliqué
+    let row = e.target.dataset.row;
+    let col = e.target.dataset.column;
+
+    // si le tir est raté on change la valeur de la case à -1 et on grise cette case pour qu'il ne puisse plus cliquer dessus
+    if (table2[row][col] == 0) {
+      //e.target.className = "manque";
+      table2[row][col] = -1;
+
+      // si le tir fait mouche on change la valeur de la case à 2 et on la colori pour montrer qu'il y avait bien une partie d'un bateau à cette case
+      //si le tir touche alors on incrémente le nombre de coup
+    } else if (table2[row][col] == 1) {
+      //e.target.className = "touche";
+      table2[row][col] = 2;
+      nombreDeCoups2++;
+      
+      //teste si le bateau a été entièrement touché, et si oui, le coule (valeur à 3)
+      if(testCoule(table2,parseInt(row),parseInt(col))){
+        console.log("coulé");
+        coule(table2,parseInt(row),parseInt(col));
+        //e.target.className = "coule";
+      }
+
+      //va falloir changer ça par un affichage au milieu de la grille (un truc plus joli quoi)
+      if (nombreDeCoups2 == 18) {
+        alert("GG WP");
+      }
+    }
+    render2();
+  }
+
+  // si jamai on aura un problème par la suite on pourra essayé de stoper la propagation de cet évènement avec e.stopPropagation()
 }
